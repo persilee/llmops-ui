90ba6d482e0d3d34a44844f2dd5db49df87a7bd4
persilee <persilee@foxmail.com>
2025-09-19 02:25:02 +0800

    🍓 添加 tailwindcss 支持和配置arcoDesign UI 框架

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
+  <h1 class="bg-amber-600">You did it!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <a-button type="primary" class="bg-amber-400!">Primary</a-button>
 </template>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,15 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import '@/assets/styles/main.css'
 
+// 创建Vue应用实例
 const app = createApp(App)
 
+// 使用Pinia状态管理插件
 app.use(createPinia())
+// 使用Vue Router路由插件
 app.use(router)
 
+// 挂载应用到DOM中id为'app'的元素
 app.mount('#app')
